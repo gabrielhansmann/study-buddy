59d66f42948e55483d652839d1803c2ab91d284b
Max Schild <schild@uni-koblenz.de>
2025-04-23 23:56:57 +0200

    Add Formik Example Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import FormikExamplePage from "./pages/FormikExamplePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/formik-example-page" element={<FormikExamplePage />} />
       </Routes>
     </Router>
   );
